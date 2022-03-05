94438196d3b6e06e4dbded95d1b6592bfebe8870
Moein Latifi <moeinl1090@protonmail.com>
2022-03-05 12:32:55 +0330

    added transaction to navigate between fragments with bottom navigation

--- app/src/main/java/ir/ariyana/wikipedia/MainActivity.kt
+++ app/src/main/java/ir/ariyana/wikipedia/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
 import ir.ariyana.wikipedia.databinding.ActivityMainBinding
+import ir.ariyana.wikipedia.fragments.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -63,5 +65,48 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        onRun()
+
+        // add listener for items in bottom navigation and adjust transactions for fragments
+        binding.activityMainBottomNavigation.setOnItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.bottomNavigationExplore -> {
+                    startTransaction(R.id.activityMainFrameLayout, FragmentExplore())
+                }
+
+                R.id.bottomNavigationTrend -> {
+                    startTransaction(R.id.activityMainFrameLayout, FragmentTrend())
+                }
+
+                R.id.bottomNavigationSaved -> {
+                    startTransaction(R.id.activityMainFrameLayout, FragmentSaved())
+                }
+
+                R.id.bottomNavigationSearch -> {
+                    startTransaction(R.id.activityMainFrameLayout, FragmentSearch())
+                }
+
+                R.id.bottomNavigationProfile -> {
+                    startTransaction(R.id.activityMainFrameLayout, FragmentProfile())
+                }
+            }
+            true
+        }
+
+        // do this to stop loading the same fragment
+        binding.activityMainBottomNavigation.setOnItemReselectedListener {}
+    }
+
+    private fun startTransaction(containerViewId : Int, fragment : Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(containerViewId, fragment)
+        transaction.commit()
+    }
+
+    private fun onRun() {
+        startTransaction(R.id.activityMainFrameLayout, FragmentExplore())
+        binding.activityMainBottomNavigation.selectedItemId = R.id.bottomNavigationExplore
     }
 }
