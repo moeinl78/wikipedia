59dd93d1091175866e3eb2b1c2ab68c6032ac470
Moein Latifi <moeinl1090@protonmail.com>
2022-03-05 11:36:53 +0330

    added base .kt files for fragments

---
+++ app/src/main/java/ir/ariyana/wikipedia/fragments/FragmentSearch.kt
@@ -0,0 +1,25 @@
+package ir.ariyana.wikipedia.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ir.ariyana.wikipedia.databinding.FragmentSearchBinding
+
+class FragmentSearch : Fragment() {
+
+    lateinit var binding : FragmentSearchBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
