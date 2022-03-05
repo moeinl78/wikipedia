1c2d8e8190eb76c4298a1aa98e2b27128da6bcd6
Moein Latifi <moeinl1090@protonmail.com>
2022-03-05 10:12:54 +0330

    added hamburger icon to the navigation view

--- app/src/main/java/ir/ariyana/wikipedia/MainActivity.kt
+++ app/src/main/java/ir/ariyana/wikipedia/MainActivity.kt
@@ -2,6 +2,8 @@ package ir.ariyana.wikipedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import ir.ariyana.wikipedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val actionBarDrawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.activityMainDrawer,
+            binding.activityMainToolbar,
+            R.string.startMenu,
+            R.string.closeMenu
+        )
+        actionBarDrawerToggle.drawerArrowDrawable.color = resources.getColor(R.color.black_200)
+
+        binding.activityMainDrawer.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+        binding.activityMainNavigationView.setNavigationItemSelectedListener {
+
+            when(it.itemId) {
+                R.id.wikibooks_item -> {
+                    Toast.makeText(this, "wikibooks", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.login_item -> {
+                    Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.writer_item -> {
+                    Toast.makeText(this, "Writer Item", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.wikidata_item -> {
+                    Toast.makeText(this, "wikidata", Toast.LENGTH_SHORT).show()
+                }
+
+                R.id.wikinews_item -> {
+                    Toast.makeText(this, "wikinews", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
