775325872c4e4b6c42e289c7b6bfb959da6228a1
Moein Latifi <moeinl1090@protonmail.com>
2022-03-07 23:28:03 +0330

    added base code for fragment info .kt

--- app/src/main/java/ir/ariyana/wikipedia/fragments/FragmentInfo.kt
+++ app/src/main/java/ir/ariyana/wikipedia/fragments/FragmentInfo.kt
@@ -1,4 +1,25 @@
 package ir.ariyana.wikipedia.fragments
 
-class FragmentInfo {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import ir.ariyana.wikipedia.databinding.FragmentInfoBinding
+
+class FragmentInfo : Fragment() {
+
+    lateinit var binding : FragmentInfoBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentInfoBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
