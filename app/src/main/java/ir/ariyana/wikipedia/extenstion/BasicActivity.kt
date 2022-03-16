7f92b1c8362b9a16d3abf240efd34c5824b5af06
Moein Latifi <moeinl1090@protonmail.com>
2022-03-17 00:06:41 +0330

    added animation for activities

---
+++ app/src/main/java/ir/ariyana/wikipedia/extenstion/BasicActivity.kt
@@ -0,0 +1,31 @@
+package ir.ariyana.wikipedia.extenstion
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import ir.ariyana.wikipedia.R
+
+open class BasicActivity : AppCompatActivity() {
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransitionExit()
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransitionExit()
+    }
+
+    override fun startActivity(intent: Intent?) {
+        super.startActivity(intent)
+        overridePendingTransitionEnter()
+    }
+
+    private fun overridePendingTransitionEnter() {
+        overridePendingTransition(R.anim.slide_in, R.anim.slide_out)
+    }
+
+    private fun overridePendingTransitionExit() {
+        overridePendingTransition(R.anim.slide_out, R.anim.slide_in)
+    }
+}
